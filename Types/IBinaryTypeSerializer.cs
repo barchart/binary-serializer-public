f315903a81de6afd43d8a492b47d8dba941e9dee
Luka Sotra <lukasotra@gmail.com>
2024-02-29 15:27:15 +0100

    Removed unnecessary whitespaces

--- Types/IBinaryTypeSerializer.cs
+++ Types/IBinaryTypeSerializer.cs
@@ -5,9 +5,7 @@ namespace Barchart.BinarySerializer.Types
     internal interface IBinaryTypeSerializer<T> : ISerializer
     {
         public void Encode(BufferHelper bufferHelper, T? value);
-
         public new HeaderWithValue Decode(BufferHelper bufferHelper);
-
         public int GetLengthInBits(T? value);
     }
 }
--- Types/ISerializer.cs
+++ Types/ISerializer.cs
@@ -5,9 +5,7 @@ namespace Barchart.BinarySerializer.Types
     internal interface ISerializer
     {
         public void Encode(BufferHelper bufferHelper,object? value);
-
         public HeaderWithValue Decode(BufferHelper bufferHelper);
-
         public int GetLengthInBits(object? value);
     }
 }
