32d3fb9bb49f0130153c28afb6ec552c1286418d
Luka Sotra <lukasotra@gmail.com>
2024-02-29 14:35:45 +0100

    Interface refactoring

---
+++ Types/IBinaryTypeSerializer.cs
@@ -0,0 +1,14 @@
+﻿using Barchart.BinarySerializer.Schemas;
+
+namespace Barchart.BinarySerializer.Types
+{
+    internal interface IBinaryTypeSerializer<T> : ISerializer
+    {
+        public void Encode(BufferHelper bufferHelper, T? value);
+
+        public new HeaderWithValue Decode(BufferHelper bufferHelper);
+
+        public int GetLengthInBits(T? value);
+    }
+}
+
--- Types/ISerializer.cs
+++ Types/ISerializer.cs
@@ -2,15 +2,6 @@
 
 namespace Barchart.BinarySerializer.Types
 {
-    internal interface IBinaryTypeSerializer<T> : ISerializer
-    {
-        public void Encode(BufferHelper bufferHelper, T? value);
-
-        public new HeaderWithValue Decode(BufferHelper bufferHelper);
-
-        public int GetLengthInBits(T? value);
-    }
-
     internal interface ISerializer
     {
         public void Encode(BufferHelper bufferHelper,object? value);
