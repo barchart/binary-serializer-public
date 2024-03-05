69a4ac172216e78098c8139abfcc04650b46c434
Luka Sotra <lukasotra@gmail.com>
2024-03-05 14:55:26 +0100

    Updated package script and removed unnecessary folders from packaging

--- Scripts/PackagePublishScript.ps1
+++ Scripts/PackagePublishScript.ps1
@@ -1,5 +1,6 @@
 dotnet build -c Release
 dotnet pack -c Release --output ./output
 
+#example of packagePath - ./output/binary-serialization-net.1.0.1.nupkg
 $packagePath = Get-ChildItem -Path ./output -Filter *.nupkg -Recurse | Select-Object -First 1
 dotnet nuget push $packagePath.FullName --api-key YourApiKey --source https://api.nuget.org/v3/index.json
