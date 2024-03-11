5ad37289bd91001c54b5ba697bd8cb149c607252
Luka Sotra <lukasotra@gmail.com>
2024-03-11 12:10:07 +0100

    Added multithreading unit tests

---
+++ Barchart.BinarySerializer/Tests/MultiThreadSerializationUnitTests.cs
@@ -0,0 +1,100 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace Barchart.BinarySerializer.Tests
+{
+    public class MultiThreadSerializationUnitTests
+	{
+        private readonly ITestOutputHelper output;
+
+        public MultiThreadSerializationUnitTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void MultiThreadSingleSerializationTest()
+        {
+            try
+            {
+                int numberOfThreads = Environment.ProcessorCount;
+
+                var testInstance = new SingleSerializationUnitTests(output);
+
+                Parallel.For(0, numberOfThreads, index =>
+                {
+                    testInstance.SerializationTest();
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        [Fact]
+        public void MultiThreadClassPropertiesSingleSerializationTest()
+        {
+            try
+            {
+                int numberOfThreads = Environment.ProcessorCount;
+
+                var testInstance = new SingleSerializationUnitTests(output);
+
+                Parallel.For(0, numberOfThreads, index =>
+                {
+                    testInstance.ClassPropertiesSerializationTest();
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        [Fact]
+        public void MultiThreadClassPropertiesDifferenceSerializationTest()
+        {
+            try
+            {
+                int numberOfThreads = Environment.ProcessorCount;
+
+                var testInstance = new DifferenceSerializationUnitTests(output);
+
+                Parallel.For(0, numberOfThreads, index =>
+                {
+                    testInstance.ClassPropertiesDifferenceSerializationTest();
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+
+        [Fact]
+        public void MultiThreadPropertyDifferenceSerializationTest()
+        {
+            try
+            {
+                int numberOfThreads = Environment.ProcessorCount;
+
+                var testInstance = new DifferenceSerializationUnitTests(output);
+
+                Parallel.For(0, numberOfThreads, index =>
+                {
+                    testInstance.PropertyDifferenceSerializationTest();
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
+
