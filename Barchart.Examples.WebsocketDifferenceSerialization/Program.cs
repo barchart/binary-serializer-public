637c6b16d22582ece5a362329e1eaa82bf5abae1
Luka Sotra <lukasotra@gmail.com>
2024-04-06 03:05:51 +0200

    Updated example for difference serialization

--- Barchart.Examples.WebsocketDifferenceSerialization/Program.cs
+++ Barchart.Examples.WebsocketDifferenceSerialization/Program.cs
@@ -1,7 +1,7 @@
-﻿using System.Globalization;
-using System.Net.WebSockets;
+﻿using System.Net.WebSockets;
 using Barchart.BinarySerializer.Schemas;
 using Barchart.SerializationData;
+using Org.Openfeed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,68 +22,57 @@ app.Map("/ws", async context => {
     if (context.WebSockets.IsWebSocketRequest)
     {
         using var ws = await context.WebSockets.AcceptWebSocketAsync();
-        Schema<StockData> stockDataSchema = SchemaFactory.GetSchema<StockData>();
+        Schema<MarketData> marketDataSchema = SchemaFactory.GetSchema<MarketData>();
 
-        StockData oldStockData = new StockData();
+        MarketData oldMarketData = new();
 
         while (true)
         {
-            Random random = new Random();
+            Random random = new();
 
-            StockData newStockData = new StockData()
+            string[] symbols = { "AAPL", "TSLA", "GOOGL", "MSFT", "AMZN" };
+            string[] descriptions = { "Apple Inc.", "Tesla, Inc.", "Alphabet Inc.", "Microsoft Corporation", "Amazon.com, Inc." };
+
+            MarketData newMarketData = new()
             {
-                Symbol = "TSLA",
-                Synthetic = random.Next(2) == 0,
-                Online = true,
-                Active = true,
-                Sequence = random.Next(),
-                Mode = random.NextDouble() < 0.5 ? "I" : "II",
-                DayNum = random.Next(1, 100),
-                BidPrice = random.NextDouble() * 200,
-                BidSize = random.Next(1, 100),
-                AskPrice = random.NextDouble() * 200,
-                AskSize = random.Next(1, 100),
-                TradePrice = random.NextDouble() * 200,
-                TradeSize = random.Next(1, 1000),
-                NumberOfTrades = random.Next(10000),
-                Vwap1 = random.NextDouble() * 200,
-                OpenPrice = random.NextDouble() * 200,
-                VolumeSpecial = random.Next(),
-                PriceChange = random.NextDouble() * 10,
-                PercentChange = random.NextDouble(),
-                LastPriceDirection = random.NextDouble() < 0.5 ? "up" : "down",
-                Time = DateTime.UtcNow,
-                TimeActual = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
-                TimeDateDisplayLong = DateTime.UtcNow.ToString("ddd, MMM d, yyyy", CultureInfo.InvariantCulture),
-                SessionDateDisplayLong = DateTime.UtcNow.ToString("ddd, MMM d, yyyy", CultureInfo.InvariantCulture),
-                PreviousTimeDateDisplayLong = DateTime.UtcNow.ToString("ddd, MMM d, yyyy", CultureInfo.InvariantCulture),
-                YesterdayDateDisplay = null,
-                TradeTime = DateTime.UtcNow,
-                TradeTimeActual = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
-                TradeTimeDisplay = DateTime.UtcNow.ToString("HH:mm tt", CultureInfo.InvariantCulture),
-                TradeDateDisplay = DateTime.UtcNow.ToString("MM/dd/yy", CultureInfo.InvariantCulture),
-                PreviousOpenPrice = random.NextDouble() * 200,
-                PreviousHighPrice = random.NextDouble() * 200,
-                PreviousLowPrice = random.NextDouble() * 200,
-                LastPriceHigh = random.NextDouble() * 200,
-                LastPriceLow = random.NextDouble() * 200,
-                Day = random.NextDouble() < 0.5 ? "K" : "P",
-                Session = random.NextDouble() < 0.5 ? "@" : "#",
-                TodayPrice = random.NextDouble() * 200,
-                PreviousPrice = random.NextDouble() * 200,
-                LastPrice = random.NextDouble() * 200,
-                LastPriceT = random.NextDouble() * 200,
-                HighPrice = random.NextDouble() * 200,
-                LowPrice = random.NextDouble() * 200,
-                TimeDateDisplay = DateTime.UtcNow.ToString("MM/dd/yy", CultureInfo.InvariantCulture),
-                TimeDisplay = DateTime.UtcNow.ToString("HH:mm tt", CultureInfo.InvariantCulture),
-                Volume = random.Next(),
-                VolumePrevious = random.Next(),
-                PriceVol = random.NextDouble() * 1_000_000_000,
-                PreviousTime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss.fffZ", CultureInfo.InvariantCulture)
+                SubscriptionResponse = new Barchart.SerializationData.SubscriptionResponse
+                {
+                    Symbol = symbols[random.Next(symbols.Length)],
+                    CorrelationId = random.Next(),
+                    MarketId = random.Next(),
+                    Exchange = "NYSE",
+                    ChannelId = random.Next(),
+                    NumberOfDefinitions = random.Next(),
+                    Unsubscribe = random.Next(0, 2) == 0 ? false : true,
+                    SnapshotIntervalSeconds = random.Next(1, 3600)
+                },
+                InstrumentDefinition = new Barchart.SerializationData.InstrumentDefinition
+                {
+                    MarketId = random.Next(),
+                    BookDepth = random.Next(),
+                    VendorId = "XYZ123",
+                    Symbol = symbols[random.Next(symbols.Length)],
+                    Description = descriptions[random.Next(descriptions.Length)]
+                },
+                MarketSnapshot = new Barchart.SerializationData.MarketSnapshot
+                {
+                    MarketId = random.Next(),
+                    TransactionTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                    MarketSequence = random.Next(),
+                    TradeDate = random.Next(20240101, 20250101)
+                },
+                MarketUpdate = new Barchart.SerializationData.MarketUpdate
+                {
+                    MarketId = random.Next(),
+                    Symbol = symbols[random.Next(symbols.Length)],
+                    TransactionTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                    DistributionTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                    MarketSequence = random.Next(),
+                    SourceSequence = random.Next()
+                }
             };
 
-            byte[] serializedData = stockDataSchema.Serialize(oldStockData, newStockData);
+            byte[] serializedData = marketDataSchema.Serialize(oldMarketData, newMarketData);
             var arraySegment = new ArraySegment<byte>(serializedData, 0, serializedData.Length);
             if (ws.State == WebSocketState.Open)
             {
@@ -94,7 +83,7 @@ app.Map("/ws", async context => {
                 break;
             }
 
-            CopyStockData(newStockData, oldStockData);
+            CopyMarketData(newMarketData, oldMarketData);
 
             Thread.Sleep(1000);
         }
@@ -112,55 +101,44 @@ app.MapFallbackToPage("/_Host");
 
 await app.RunAsync();
 
-void CopyStockData(StockData source, StockData target)
+void CopyMarketData(MarketData source, MarketData target)
 {
-    target.Symbol = source.Symbol;
-    target.Synthetic = source.Synthetic;
-    target.Online = source.Online;
-    target.Active = source.Active;
-    target.Sequence = source.Sequence;
-    target.Mode = source.Mode;
-    target.DayNum = source.DayNum;
-    target.BidPrice = source.BidPrice;
-    target.BidSize = source.BidSize;
-    target.AskPrice = source.AskPrice;
-    target.AskSize = source.AskSize;
-    target.TradePrice = source.TradePrice;
-    target.TradeSize = source.TradeSize;
-    target.NumberOfTrades = source.NumberOfTrades;
-    target.Vwap1 = source.Vwap1;
-    target.OpenPrice = source.OpenPrice;
-    target.VolumeSpecial = source.VolumeSpecial;
-    target.PriceChange = source.PriceChange;
-    target.PercentChange = source.PercentChange;
-    target.LastPriceDirection = source.LastPriceDirection;
-    target.Time = source.Time;
-    target.TimeActual = source.TimeActual;
-    target.TimeDateDisplayLong = source.TimeDateDisplayLong;
-    target.SessionDateDisplayLong = source.SessionDateDisplayLong;
-    target.PreviousTimeDateDisplayLong = source.PreviousTimeDateDisplayLong;
-    target.YesterdayDateDisplay = source.YesterdayDateDisplay;
-    target.TradeTime = source.TradeTime;
-    target.TradeTimeActual = source.TradeTimeActual;
-    target.TradeTimeDisplay = source.TradeTimeDisplay;
-    target.TradeDateDisplay = source.TradeDateDisplay;
-    target.PreviousOpenPrice = source.PreviousOpenPrice;
-    target.PreviousHighPrice = source.PreviousHighPrice;
-    target.PreviousLowPrice = source.PreviousLowPrice;
-    target.LastPriceHigh = source.LastPriceHigh;
-    target.LastPriceLow = source.LastPriceLow;
-    target.Day = source.Day;
-    target.Session = source.Session;
-    target.TodayPrice = source.TodayPrice;
-    target.PreviousPrice = source.PreviousPrice;
-    target.LastPrice = source.LastPrice;
-    target.LastPriceT = source.LastPriceT;
-    target.HighPrice = source.HighPrice;
-    target.LowPrice = source.LowPrice;
-    target.TimeDateDisplay = source.TimeDateDisplay;
-    target.TimeDisplay = source.TimeDisplay;
-    target.Volume = source.Volume;
-    target.VolumePrevious = source.VolumePrevious;
-    target.PriceVol = source.PriceVol;
-    target.PreviousTime = source.PreviousTime;
+    target.SubscriptionResponse = new Barchart.SerializationData.SubscriptionResponse
+    {
+        Symbol = source.SubscriptionResponse?.Symbol,
+        CorrelationId = source.SubscriptionResponse?.CorrelationId,
+        MarketId = source.SubscriptionResponse?.MarketId,
+        Exchange = source.SubscriptionResponse?.Exchange,
+        ChannelId = source.SubscriptionResponse?.ChannelId,
+        NumberOfDefinitions = source.SubscriptionResponse?.NumberOfDefinitions,
+        Unsubscribe = source.SubscriptionResponse?.Unsubscribe,
+        SnapshotIntervalSeconds = source.SubscriptionResponse?.SnapshotIntervalSeconds
+    };
+
+    target.InstrumentDefinition = new Barchart.SerializationData.InstrumentDefinition
+    {
+        MarketId = source.InstrumentDefinition?.MarketId,
+        BookDepth = source.InstrumentDefinition?.BookDepth,
+        VendorId = source.InstrumentDefinition?.VendorId,
+        Symbol = source.InstrumentDefinition?.Symbol,
+        Description = source.InstrumentDefinition?.Description
+    };
+
+    target.MarketSnapshot = new Barchart.SerializationData.MarketSnapshot
+    {
+        MarketId = source.MarketSnapshot?.MarketId,
+        TransactionTime = source.MarketSnapshot?.TransactionTime,
+        MarketSequence = source.MarketSnapshot?.MarketSequence,
+        TradeDate = source.MarketSnapshot?.TradeDate
+    };
+
+    target.MarketUpdate = new Barchart.SerializationData.MarketUpdate
+    {
+        MarketId = source.MarketUpdate?.MarketId,
+        Symbol = source.MarketUpdate?.Symbol,
+        TransactionTime = source.MarketUpdate?.TransactionTime,
+        DistributionTime = source.MarketUpdate?.DistributionTime,
+        MarketSequence = source.MarketUpdate?.MarketSequence,
+        SourceSequence = source.MarketUpdate?.SourceSequence
+    };
 }
