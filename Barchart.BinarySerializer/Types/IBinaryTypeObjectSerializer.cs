5fff2fd2e81d3bc86dc9e8eb1b46861548a5f8f1
Luka Sotra <lukasotra@gmail.com>
2024-04-10 12:51:41 +0200

    Added object serializer interface

---
+++ Barchart.BinarySerializer/Types/IBinaryTypeObjectSerializer.cs
@@ -0,0 +1,14 @@
+﻿using Barchart.BinarySerializer.Schemas;
+
+namespace Barchart.BinarySerializer.Types
+{
+    /// <summary>
+    /// Provides additional methods for encoding and decoding values of type <typeparamref name="T"/> to and from a <see cref="DataBuffer"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to be serialized.</typeparam>
+    public interface IBinaryTypeObjectSerializer<T> : IBinaryTypeSerializer<T>
+    {
+        public void Encode(DataBuffer dataBuffer, T? oldValue, T? newValue);
+        public HeaderWithValue<T> Decode(DataBuffer dataBuffer, T existing);
+    }
+}
