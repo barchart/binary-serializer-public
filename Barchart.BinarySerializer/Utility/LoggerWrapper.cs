3242920e533fe5df191dd2b2127de8a80039337a
Luka Sotra <lukasotra@gmail.com>
2024-04-15 15:42:17 +0200

    Added logger wrapper class

---
+++ Barchart.BinarySerializer/Utility/LoggerWrapper.cs
@@ -0,0 +1,38 @@
+﻿using Serilog;
+namespace Barchart.BinarySerializer.Utility
+{
+    /// <summary>
+    /// Provides utility methods for logging using Serilog.
+    /// </summary>
+    public static class LoggerWrapper
+    {
+        public static void InitializeLogger()
+        {
+            string logFilePath = "../../../../Logs/log-.txt";
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+                .CreateLogger();
+        }
+
+        public static void LogInformation(string text)
+        {
+            Log.Information(text);
+        }
+
+        public static void LogDebug(string text)
+        {
+            Log.Debug(text);
+        }
+
+        public static void LogWarning(string text)
+        {
+            Log.Warning(text);
+        }
+
+        public static void LogError(string text)
+        {
+            Log.Error(text);
+        }
+    }
+}
