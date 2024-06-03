b54332f4c4a0ae79839224e39ff0794d72b3b173
Luka Sotra <lukasotra@gmail.com>
2024-06-04 01:49:41 +0200

    Updated unit test

--- Barchart.BinarySerializer/Tests/DifferenceSerializationUnitTests.cs
+++ Barchart.BinarySerializer/Tests/DifferenceSerializationUnitTests.cs
@@ -268,7 +268,7 @@ namespace Barchart.BinarySerializer.Tests
 
             Garage garage2 = new()
             {
-                CarObject = null,
+                CarObject = car2,
                 PersonObject = person2,
                 StringNameGarage = "Garage",
                 DecimalNumberGarage = 122.5m,
