0aaa57b351de3517219d402713f7b9937b4e34d6
Bryan Ingle <bryaningle@gmail.com>
2024-06-24 17:53:21 -0600

    Formatting: Change `namespace` usage

--- Barchart.BinarySerializer.Tests/Buffers/DataBuffer.cs
+++ Barchart.BinarySerializer.Tests/Buffers/DataBuffer.cs
@@ -4,209 +4,210 @@ using Barchart.BinarySerializer.Buffers;
 
 #endregion
 
-namespace Barchart.BinarySerializer.Tests.Buffers;
-
-public class DataBufferTests
+namespace Barchart.BinarySerializer.Tests.Buffers
 {
-    #region Fields
-    
-    private readonly ITestOutputHelper _testOutputHelper;
-    
-    #endregion
-    
-    #region Constructor(s)
-    
-    public DataBufferTests(ITestOutputHelper testOutputHelper)
+    public class DataBufferTests
     {
-        _testOutputHelper = testOutputHelper;
-    }
-    
-    #endregion
-    
-    #region Test Methods (ReadByte)
+        #region Fields
 
-    [Fact]
-    public void ReadByte_Once_ReturnsFirstByte()
-    {
-        byte first;
-        
-        var byteArray = new [] { first = 250 };
-        var dataBuffer = new DataBuffer(byteArray);
+        private readonly ITestOutputHelper _testOutputHelper;
 
-        byte readFirst = dataBuffer.ReadByte();
-        
-        Assert.Equal(first, readFirst);
-    }
-    
-    [Fact]
-    public void ReadByte_Twice_ReturnsSecondByte()
-    {
-        byte first;
-        byte second;
+        #endregion
 
-        var byteArray = new [] { first = 250, second = 175 };
-        var dataBuffer = new DataBuffer(byteArray);
+        #region Constructor(s)
 
-        byte readFirst = dataBuffer.ReadByte();
-        byte readSecond = dataBuffer.ReadByte();
-        
-        Assert.Equal(first, readFirst);
-        Assert.Equal(second, readSecond);
-    }
-    
-    #endregion
-    
-    #region Test Methods (WriteBit)
-    
-    [Fact]
-    public void WriteBit_True_ModifiesBuffer()
-    {
-        var byteArray = new byte[1];
-        var dataBuffer = new DataBuffer(byteArray);
-        
-        dataBuffer.WriteBit(true);
-        
-        Assert.True(GetBit(byteArray[0], 0));
-    }
-    
-    [Fact]
-    public void WriteBit_False_ModifiesBuffer()
-    {
-        var byteArray = new byte[1];
-        var dataBuffer = new DataBuffer(byteArray);
-        
-        dataBuffer.WriteBit(false);
-        
-        Assert.False(GetBit(byteArray[0], 0));
-    }
-    
-    [Theory]
-    [InlineData(new[] { true, true })]
-    [InlineData(new[] { false, false })]
-    [InlineData(new[] { true, true, false, true })]
-    [InlineData(new[] { false, false, true, false })]
-    [InlineData(new[] { true, false, true, false, true, false, true, false })]
-    [InlineData(new[] { false, true, false, true, false, true, false, true })]
-    public void WriteBit_Multiple_ModifiesBuffer(bool[] bits)
-    {
-        var byteArray = new byte[1];
-        var dataBuffer = new DataBuffer(byteArray);
-        
-        for (int i = 0; i < bits.Length; i++)
+        public DataBufferTests(ITestOutputHelper testOutputHelper)
         {
-            dataBuffer.WriteBit(bits[i]);
-        }
-        
-        _testOutputHelper.WriteLine(PrintBits(byteArray[0]));
-        
-        for (int i = 0; i < bits.Length; i++)
-        {
-            Assert.Equal(GetBit(byteArray[0], i), bits[i]);
-        }
-    }
-
-    #endregion
-
-    #region Test Methods (ToBytes)
-    
-    [Fact]
-    public void ToBytes_WhenZeroBitsAreWritten_ReturnsEmptyArray()
-    {
-        var byteArray = new byte[2];
-        var dataBuffer = new DataBuffer(byteArray);
-
-        var bytes = dataBuffer.ToBytes();
-
-        Assert.Empty(bytes);
-    }
-    
-    [Fact]
-    public void ToBytes_WhenOneBitIsWritten_ReturnsOneByteArray()
-    {
-        var byteArray = new byte[2];
-        var dataBuffer = new DataBuffer(byteArray);
-        
-        for (int i = 0; i < 1; i++)
-        {
-            dataBuffer.WriteBit(i % 2 == 0);
-        }       
-        
-        var bytes = dataBuffer.ToBytes();
-
-        Assert.Single(bytes);
-    }
-
-    [Fact]
-    public void ToBytes_WhenOneByteIsWritten_ReturnsOneByteArray()
-    {
-        var byteArray = new byte[2];
-        var dataBuffer = new DataBuffer(byteArray);
-        
-        for (int i = 0; i < 8; i++)
-        {
-            dataBuffer.WriteBit(i % 2 == 0);
+            _testOutputHelper = testOutputHelper;
         }
 
-        var bytes = dataBuffer.ToBytes();
+        #endregion
 
-        Assert.Single(bytes);
-    }
-    
-    [Fact]
-    public void ToBytes_WhenTwelveBitsAreWritten_ReturnsTwoBytesArray()
-    {
-        var byteArray = new byte[2];
-        var dataBuffer = new DataBuffer(byteArray);
-        
-        for (int i = 0; i < 12; i++)
+        #region Test Methods (ReadByte)
+
+        [Fact]
+        public void ReadByte_Once_ReturnsFirstByte()
         {
-            dataBuffer.WriteBit(i % 2 == 0);
+            byte first;
+
+            var byteArray = new[] { first = 250 };
+            var dataBuffer = new DataBuffer(byteArray);
+
+            byte readFirst = dataBuffer.ReadByte();
+
+            Assert.Equal(first, readFirst);
         }
 
-        var bytes = dataBuffer.ToBytes();
-
-        Assert.Equal(2, bytes.Length);
-    }
-
-    [Fact]
-    public void ToBytes_WhenTwoBytesAreWritten_ReturnsTwoBytesArray()
-    {
-        var byteArray = new byte[2];
-        var dataBuffer = new DataBuffer(byteArray);
-        
-        for (int i = 0; i < 16; i++)
+        [Fact]
+        public void ReadByte_Twice_ReturnsSecondByte()
         {
-            dataBuffer.WriteBit(i % 2 == 0);
-        }
-        
-        var bytes = dataBuffer.ToBytes();
+            byte first;
+            byte second;
 
-        Assert.Equal(2, bytes.Length);
-    }
+            var byteArray = new[] { first = 250, second = 175 };
+            var dataBuffer = new DataBuffer(byteArray);
 
-    #endregion
-    
-    #region Static Methods
-    
-    private static bool GetBit(byte b, int index)
-    {
-        if (index < 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(index), index, "Bit index cannot be less than zero.");
-        }
-        
-        if (index > 7)
-        {
-            throw new ArgumentOutOfRangeException(nameof(index), index, "Bit index cannot be greater than seven.");
+            byte readFirst = dataBuffer.ReadByte();
+            byte readSecond = dataBuffer.ReadByte();
+
+            Assert.Equal(first, readFirst);
+            Assert.Equal(second, readSecond);
         }
 
-        return ((b >> (7 - index)) & 1) == 1;
-    }
+        #endregion
 
-    private static string PrintBits(byte b)
-    {
-        return Convert.ToString(b, 2).PadLeft(8, '0');
+        #region Test Methods (WriteBit)
+
+        [Fact]
+        public void WriteBit_True_ModifiesBuffer()
+        {
+            var byteArray = new byte[1];
+            var dataBuffer = new DataBuffer(byteArray);
+
+            dataBuffer.WriteBit(true);
+
+            Assert.True(GetBit(byteArray[0], 0));
+        }
+
+        [Fact]
+        public void WriteBit_False_ModifiesBuffer()
+        {
+            var byteArray = new byte[1];
+            var dataBuffer = new DataBuffer(byteArray);
+
+            dataBuffer.WriteBit(false);
+
+            Assert.False(GetBit(byteArray[0], 0));
+        }
+
+        [Theory]
+        [InlineData(new[] { true, true })]
+        [InlineData(new[] { false, false })]
+        [InlineData(new[] { true, true, false, true })]
+        [InlineData(new[] { false, false, true, false })]
+        [InlineData(new[] { true, false, true, false, true, false, true, false })]
+        [InlineData(new[] { false, true, false, true, false, true, false, true })]
+        public void WriteBit_Multiple_ModifiesBuffer(bool[] bits)
+        {
+            var byteArray = new byte[1];
+            var dataBuffer = new DataBuffer(byteArray);
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                dataBuffer.WriteBit(bits[i]);
+            }
+
+            _testOutputHelper.WriteLine(PrintBits(byteArray[0]));
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                Assert.Equal(GetBit(byteArray[0], i), bits[i]);
+            }
+        }
+
+        #endregion
+
+        #region Test Methods (ToBytes)
+
+        [Fact]
+        public void ToBytes_WhenZeroBitsAreWritten_ReturnsEmptyArray()
+        {
+            var byteArray = new byte[2];
+            var dataBuffer = new DataBuffer(byteArray);
+
+            var bytes = dataBuffer.ToBytes();
+
+            Assert.Empty(bytes);
+        }
+
+        [Fact]
+        public void ToBytes_WhenOneBitIsWritten_ReturnsOneByteArray()
+        {
+            var byteArray = new byte[2];
+            var dataBuffer = new DataBuffer(byteArray);
+
+            for (int i = 0; i < 1; i++)
+            {
+                dataBuffer.WriteBit(i % 2 == 0);
+            }
+
+            var bytes = dataBuffer.ToBytes();
+
+            Assert.Single(bytes);
+        }
+
+        [Fact]
+        public void ToBytes_WhenOneByteIsWritten_ReturnsOneByteArray()
+        {
+            var byteArray = new byte[2];
+            var dataBuffer = new DataBuffer(byteArray);
+
+            for (int i = 0; i < 8; i++)
+            {
+                dataBuffer.WriteBit(i % 2 == 0);
+            }
+
+            var bytes = dataBuffer.ToBytes();
+
+            Assert.Single(bytes);
+        }
+
+        [Fact]
+        public void ToBytes_WhenTwelveBitsAreWritten_ReturnsTwoBytesArray()
+        {
+            var byteArray = new byte[2];
+            var dataBuffer = new DataBuffer(byteArray);
+
+            for (int i = 0; i < 12; i++)
+            {
+                dataBuffer.WriteBit(i % 2 == 0);
+            }
+
+            var bytes = dataBuffer.ToBytes();
+
+            Assert.Equal(2, bytes.Length);
+        }
+
+        [Fact]
+        public void ToBytes_WhenTwoBytesAreWritten_ReturnsTwoBytesArray()
+        {
+            var byteArray = new byte[2];
+            var dataBuffer = new DataBuffer(byteArray);
+
+            for (int i = 0; i < 16; i++)
+            {
+                dataBuffer.WriteBit(i % 2 == 0);
+            }
+
+            var bytes = dataBuffer.ToBytes();
+
+            Assert.Equal(2, bytes.Length);
+        }
+
+        #endregion
+
+        #region Static Methods
+
+        private static bool GetBit(byte b, int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), index, "Bit index cannot be less than zero.");
+            }
+
+            if (index > 7)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), index, "Bit index cannot be greater than seven.");
+            }
+
+            return ((b >> (7 - index)) & 1) == 1;
+        }
+
+        private static string PrintBits(byte b)
+        {
+            return Convert.ToString(b, 2).PadLeft(8, '0');
+        }
+
+        #endregion
     }
-    
-    #endregion
 }
