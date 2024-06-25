9b7b9af922a93a641c3966488c2da6426b5a27bf
Luka Sotra <lukasotra@gmail.com>
2024-06-25 19:00:24 +0200

    Refactor: Docs.

--- Barchart.BinarySerializer/Schemas/BinarySerializeAttribute.cs
+++ Barchart.BinarySerializer/Schemas/BinarySerializeAttribute.cs
@@ -9,8 +9,18 @@
     {
         #region Properties
 
+        /// <summary>
+        ///     Indicates whether the property or field is a key.
+        ///     A key property or field is used to uniquely identify an object instance.
+        /// </summary>
         public bool Key { get; }
 
+        /// <summary>
+        ///     Indicates whether the property or field should be included in serialization.
+        ///     If set to true, the property or field will be included in the serialized output.
+        ///     If set to false, the property or field will be excluded from the serialized output.
+        ///     Default value is true.
+        /// </summary>
         public bool Include { get; }
 
         #endregion
