24e7a6e7e9b83c11e96bebefe8d5baea80fbc870
Bryan Ingle <bryaningle@gmail.com>
2024-07-02 13:41:28 -0600

    Feature: Implement the default IDataBufferWriterFactory (using ThreadStatic byte array)

---
+++ Barchart.BinarySerializer/Buffers/Factories/DataBufferWriterFactory.cs
@@ -0,0 +1,52 @@
+namespace Barchart.BinarySerializer.Buffers.Factories;
+
+/// <summary>
+///     An implementation of <see cref="IDataBufferWriterFactory" /> that uses
+///     thead-static byte arrays as the underlying binary data storage for
+///     each <see cref="IDataBufferWriter" />.
+/// </summary>
+public class DataBufferWriterFactory : IDataBufferWriterFactory
+{
+    #region Constants
+
+    private const int DEFAULT_BYTE_ARRAY_LENGTH = 512 * 1024;
+    
+    #endregion
+    
+    #region Fields
+    
+    [ThreadStatic]
+    private static byte[]? _byteArray;
+
+    private int _byteArrayLength;
+    
+    #endregion
+    
+    #region Constructor(s)
+
+    public DataBufferWriterFactory() : this(DEFAULT_BYTE_ARRAY_LENGTH)
+    {
+        
+    }
+    
+    public DataBufferWriterFactory(int byteArrayLength)
+    {
+        _byteArrayLength = byteArrayLength;
+    }
+    
+    #endregion
+
+    #region Methods
+
+    public IDataBufferWriter GetDataBufferWriter()
+    {
+        if (_byteArray == null)
+        {
+            _byteArray = new byte[_byteArrayLength];
+        }
+
+        return new DataBufferWriter(_byteArray);
+    }
+    
+    #endregion
+}
--- Barchart.BinarySerializer/Buffers/Factories/IDataBufferWriterFactory.cs
+++ Barchart.BinarySerializer/Buffers/Factories/IDataBufferWriterFactory.cs
@@ -1,6 +1,12 @@
 namespace Barchart.BinarySerializer.Buffers.Factories;
 
+/// <summary>
+///     A strategy for creating <see cref="IDataBufferWriter" /> instances.
+/// </summary>
 public interface IDataBufferWriterFactory
 {
-    
+    /// <summary>
+    ///     Creates a <see cref="IDataBufferWriter" /> instance.
+    /// </summary>
+    IDataBufferWriter GetDataBufferWriter();
 }
