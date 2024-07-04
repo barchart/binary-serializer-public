5ab678033aa9afc58d41002fc648c22515f03717
Luka Sotra <lukasotra@gmail.com>
2024-07-04 12:38:25 +0200

    Refactor: Docs.

--- Barchart.BinarySerializer/Schemas/Factories/ISchemaFactory.cs
+++ Barchart.BinarySerializer/Schemas/Factories/ISchemaFactory.cs
@@ -1,6 +1,23 @@
 namespace Barchart.BinarySerializer.Schemas.Factories;
 
+/// <summary>
+///     Defines a factory for creating schemas for entities.
+/// </summary>
 public interface ISchemaFactory
 {
+    /// <summary>
+    ///     Creates a schema for the specified entity type.
+    /// </summary>
+    /// <typeparam name="TEntity">
+    ///     The type of entity for which to create the schema.
+    /// </typeparam>
+    /// <returns>
+    ///     An <see cref="ISchema{TEntity}"/> representing the schema for the entity.
+    /// </returns>
+    /// <remarks>
+    ///     This method analyzes the properties of the entity type and creates a schema based on
+    ///     properties marked with the [Serialize] attribute. The resulting schema can be used
+    ///     for binary serialization and deserialization of the entity.
+    /// </remarks>
     ISchema<TEntity> Make<TEntity>() where TEntity: new();
 }
