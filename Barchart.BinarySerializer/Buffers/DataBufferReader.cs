fd8ee7a2791ca91aaba3ce1a13604afa70683904
Luka Sotra <lukasotra@gmail.com>
2024-07-10 12:06:59 +0200

    Refactor: Tweak.

--- Barchart.BinarySerializer/Buffers/DataBufferReader.cs
+++ Barchart.BinarySerializer/Buffers/DataBufferReader.cs
@@ -1,113 +1,112 @@
-namespace Barchart.BinarySerializer.Buffers
+namespace Barchart.BinarySerializer.Buffers;
+
+/// <summary>
+///     A data buffer reader which uses a fixed-length byte array
+///     for internal storage.
+/// </summary>
+public class DataBufferReader : IDataBufferReader
 {
+    #region Fields
+
+    private readonly byte[] _byteArray;
+
+    private int _positionByte;
+    private int _positionBit;
+
+    private const byte TRUE = 1;
+
+    #endregion
+
+    #region Constructor(s)
+
     /// <summary>
-    ///     A data buffer reader which uses a fixed-length byte array
-    ///     for internal storage.
+    ///     Creates a new <see cref="DataBufferReader"/> instance.
     /// </summary>
-    public class DataBufferReader : IDataBufferReader
+    /// <param name="byteArray">
+    ///     The internal storage mechanism for the class.
+    /// </param>
+    public DataBufferReader(byte[] byteArray)
     {
-        #region Fields
+        _byteArray = byteArray;
 
-        private readonly byte[] _byteArray;
-
-        private int _positionByte;
-        private int _positionBit;
-
-        private const byte TRUE = 1;
-
-        #endregion
-
-        #region Constructor(s)
-
-        /// <summary>
-        ///     Creates a new <see cref="DataBufferReader"/> instance.
-        /// </summary>
-        /// <param name="byteArray">
-        ///     The internal storage mechanism for the class.
-        /// </param>
-        public DataBufferReader(byte[] byteArray)
-        {
-            _byteArray = byteArray;
-
-            _positionByte = 0;
-            _positionBit = 0;
-        }
-
-        #endregion
-
-        #region Methods
-
-        /// <inheritdoc />
-        public bool ReadBit()
-        {
-            if (IsBufferFull())
-            {
-                throw new InvalidOperationException("Unable to read bit. The buffer is currently positioned at the end of the internal byte array.");
-            }
-
-            int bit = (_byteArray[_positionByte] >> (7 - _positionBit)) & 1;
-
-            AdvanceBit();
-
-            return bit == 1;
-        }
-
-        /// <inheritdoc />
-        public byte ReadByte()
-        {
-            byte value = 0;
-
-            for (int j = 7; j >= 0; j--)
-            {
-                bool bit = ReadBit();
-
-                if (bit)
-                {
-                    value = (byte)(value | (TRUE << j));
-                }
-            }
-
-            return value;
-        }
-
-        /// <inheritdoc />
-        public byte[] ReadBytes(int size)
-        {
-            byte[] bytes = new byte[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                bytes[i] = ReadByte();
-            }
-
-            return bytes;
-        }
-
-        /// <inheritdoc />
-        public void Reset()
-        {
-            _positionByte = 0;
-            _positionBit = 0;
-        }
-
-        private void AdvanceBit()
-        {
-            if (_positionBit == 7)
-            {
-                _positionBit = 0;
-                _positionByte++;
-            }
-            else
-            {
-                _positionBit++;
-            }
-        }
-
-        private bool IsBufferFull()
-        {
-            return _positionByte >= _byteArray.Length;
-        }
-
-        #endregion
+        _positionByte = 0;
+        _positionBit = 0;
     }
+
+    #endregion
+
+    #region Methods
+
+    /// <inheritdoc />
+    public bool ReadBit()
+    {
+        if (IsBufferFull())
+        {
+            throw new InvalidOperationException("Unable to read bit. The buffer is currently positioned at the end of the internal byte array.");
+        }
+
+        int bit = (_byteArray[_positionByte] >> (7 - _positionBit)) & 1;
+
+        AdvanceBit();
+
+        return bit == 1;
+    }
+
+    /// <inheritdoc />
+    public byte ReadByte()
+    {
+        byte value = 0;
+
+        for (int j = 7; j >= 0; j--)
+        {
+            bool bit = ReadBit();
+
+            if (bit)
+            {
+                value = (byte)(value | (TRUE << j));
+            }
+        }
+
+        return value;
+    }
+
+    /// <inheritdoc />
+    public byte[] ReadBytes(int size)
+    {
+        byte[] bytes = new byte[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            bytes[i] = ReadByte();
+        }
+
+        return bytes;
+    }
+
+    /// <inheritdoc />
+    public void Reset()
+    {
+        _positionByte = 0;
+        _positionBit = 0;
+    }
+
+    private void AdvanceBit()
+    {
+        if (_positionBit == 7)
+        {
+            _positionBit = 0;
+            _positionByte++;
+        }
+        else
+        {
+            _positionBit++;
+        }
+    }
+
+    private bool IsBufferFull()
+    {
+        return _positionByte >= _byteArray.Length;
+    }
+
+    #endregion
 }
--- Barchart.BinarySerializer/Buffers/DataBufferWriter.cs
+++ Barchart.BinarySerializer/Buffers/DataBufferWriter.cs
@@ -1,115 +1,114 @@
-namespace Barchart.BinarySerializer.Buffers
+namespace Barchart.BinarySerializer.Buffers;
+
+/// <summary>
+///     A data buffer writer which uses a fixed-length byte array
+///     for internal storage.
+/// </summary>
+public class DataBufferWriter : IDataBufferWriter
 {
+    #region Fields
+
+    private readonly byte[] _byteArray;
+
+    private int _positionByte;
+    private int _positionBit;
+
+    private const byte TRUE = 1;
+
+    #endregion
+
+    #region Constructor(s)
+
     /// <summary>
-    ///     A data buffer writer which uses a fixed-length byte array
-    ///     for internal storage.
+    ///     Creates a new <see cref="DataBufferWriter"/> instance.
     /// </summary>
-    public class DataBufferWriter : IDataBufferWriter
+    /// <param name="byteArray">
+    ///     The internal storage mechanism for the class. Write operations will mutate this array.
+    /// </param>
+    public DataBufferWriter(byte[] byteArray)
     {
-        #region Fields
+        _byteArray = byteArray;
 
-        private readonly byte[] _byteArray;
+        _positionByte = 0;
+        _positionBit = 0;
+    }
 
-        private int _positionByte;
-        private int _positionBit;
+    #endregion
 
-        private const byte TRUE = 1;
+    #region Methods
 
-        #endregion
-
-        #region Constructor(s)
-
-        /// <summary>
-        ///     Creates a new <see cref="DataBufferWriter"/> instance.
-        /// </summary>
-        /// <param name="byteArray">
-        ///     The internal storage mechanism for the class. Write operations will mutate this array.
-        /// </param>
-        public DataBufferWriter(byte[] byteArray)
+    /// <inheritdoc />
+    public void WriteBit(bool value)
+    {
+        if (IsBufferFull())
         {
-            _byteArray = byteArray;
-
-            _positionByte = 0;
-            _positionBit = 0;
+            throw new InvalidOperationException("Unable to write bit. The buffer is currently positioned at the end of the internal byte array.");
         }
 
-        #endregion
-
-        #region Methods
-
-        /// <inheritdoc />
-        public void WriteBit(bool value)
+        if (value)
         {
-            if (IsBufferFull())
+            byte byteCurrent;
+
+            if (_positionBit == 0)
             {
-                throw new InvalidOperationException("Unable to write bit. The buffer is currently positioned at the end of the internal byte array.");
-            }
-
-            if (value)
-            {
-                byte byteCurrent;
-
-                if (_positionBit == 0)
-                {
-                    byteCurrent = 0;
-                }
-                else
-                {
-                    byteCurrent = _byteArray[_positionByte];
-                }
-
-                byte byteMask = (byte)(TRUE << (7 - _positionBit));
-
-                _byteArray[_positionByte] = (byte)(byteCurrent | byteMask);
-            }
-
-            AdvanceBit();
-        }
-
-        /// <inheritdoc />
-        public void WriteByte(byte value)
-        {
-            for (int j = 7; j >= 0; j--)
-            {
-                WriteBit(((value >> j) & 1) == 1);
-            }
-        }
-
-        /// <inheritdoc />
-        public void WriteBytes(byte[] value)
-        {
-            for (int i = 0; i < value.Length; i++)
-            {
-                WriteByte(value[i]);
-            }
-        }
-
-        /// <inheritdoc />
-        public byte[] ToBytes()
-        {
-            int byteCount = _positionByte + (_positionBit == 0 ? 0 : 1);
-
-            return _byteArray.Take(byteCount).ToArray();
-        }
-
-        private void AdvanceBit()
-        {
-            if (_positionBit == 7)
-            {
-                _positionBit = 0;
-                _positionByte++;
+                byteCurrent = 0;
             }
             else
             {
-                _positionBit++;
+                byteCurrent = _byteArray[_positionByte];
             }
+
+            byte byteMask = (byte)(TRUE << (7 - _positionBit));
+
+            _byteArray[_positionByte] = (byte)(byteCurrent | byteMask);
         }
 
-        private bool IsBufferFull()
-        {
-            return _positionByte >= _byteArray.Length;
-        }
-
-        #endregion
+        AdvanceBit();
     }
+
+    /// <inheritdoc />
+    public void WriteByte(byte value)
+    {
+        for (int j = 7; j >= 0; j--)
+        {
+            WriteBit(((value >> j) & 1) == 1);
+        }
+    }
+
+    /// <inheritdoc />
+    public void WriteBytes(byte[] value)
+    {
+        for (int i = 0; i < value.Length; i++)
+        {
+            WriteByte(value[i]);
+        }
+    }
+
+    /// <inheritdoc />
+    public byte[] ToBytes()
+    {
+        int byteCount = _positionByte + (_positionBit == 0 ? 0 : 1);
+
+        return _byteArray.Take(byteCount).ToArray();
+    }
+
+    private void AdvanceBit()
+    {
+        if (_positionBit == 7)
+        {
+            _positionBit = 0;
+            _positionByte++;
+        }
+        else
+        {
+            _positionBit++;
+        }
+    }
+
+    private bool IsBufferFull()
+    {
+        return _positionByte >= _byteArray.Length;
+    }
+
+    #endregion
 }
