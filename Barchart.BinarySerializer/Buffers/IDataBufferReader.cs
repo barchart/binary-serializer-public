881b77fbac4105d07c6f4d2f03db1c8680bdc4bf
Luka Sotra <lukasotra@gmail.com>
2024-07-11 23:23:19 +0200

    Feat: Add `BytesWritten` method to the `IDataBufferReader` interface.

--- Barchart.BinarySerializer/Buffers/IDataBufferReader.cs
+++ Barchart.BinarySerializer/Buffers/IDataBufferReader.cs
@@ -49,5 +49,13 @@ public interface IDataBufferReader
     /// </summary>
     void Reset();
 
+     /// <summary>
+    ///     Gets the total number of bytes written to the buffer.
+    /// </summary>
+    /// <returns>
+    ///     The total number of bytes written.
+    /// </returns>
+    int BytesWritten();
+
     #endregion
 }
