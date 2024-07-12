c47f963ec3494caf68327aa4a02089d8bb650b80
Luka Sotra <lukasotra@gmail.com>
2024-07-12 13:39:42 +0200

    Test: Add tests for `Encode` method for `SchemaItem`.

--- Barchart.BinarySerializer.Tests/Schemas/SchemaItemTests.cs
+++ Barchart.BinarySerializer.Tests/Schemas/SchemaItemTests.cs
@@ -1,5 +1,10 @@
 #region Using Statements
 
+using Barchart.BinarySerializer.Schemas;
+using Barchart.BinarySerializer.Buffers;
+using Barchart.BinarySerializer.Types;
+using Barchart.BinarySerializer.Schemas.Exceptions;
+using Barchart.BinarySerializer.Attributes;
 
 #endregion
 
@@ -11,6 +16,13 @@ public class SchemaItemTests
 
     private readonly ITestOutputHelper _testOutputHelper;
 
+    private readonly byte[] _buffer;
+    private readonly IDataBufferWriter _writer;
+
+    private readonly Mock<IBinaryTypeSerializer<string>> _serializerMock;
+    private readonly Func<TestEntity, string> _getter;
+    private readonly Action<TestEntity, string> _setter;
+
     #endregion
 
     #region Constructor(s)
@@ -18,22 +30,93 @@ public class SchemaItemTests
     public SchemaItemTests(ITestOutputHelper testOutputHelper)
     {
         _testOutputHelper = testOutputHelper;
+
+        _buffer = new byte[100000]; 
+        _writer = new DataBufferWriter(_buffer);
+
+        _serializerMock = new Mock<IBinaryTypeSerializer<string>>();
+        _getter = entity => entity.Name;
+        _setter = (entity, value) => entity.Name = value;
     }
 
     #endregion
 
     #region Test Methods (Encode)
 
-
-    #endregion
-
-    #region Test Methods (Decode)
+    [Fact]
+    public void Encode_WithValidData_WritesData()
+    {
+        SchemaItem<TestEntity, string> schemaItem = new("Name", true, _getter, _setter, _serializerMock.Object);
+        
+        TestEntity current = new()
+        {
+            Name = "CurrentValue"
+        };
     
+        schemaItem.Encode(_writer, current);
+    
+        _serializerMock.Verify(s => s.Encode(It.IsAny<IDataBufferWriter>(), It.Is<string>(p => p == "CurrentValue")), Times.Once);
+        _serializerMock.Verify(s => s.Encode(It.IsAny<IDataBufferWriter>(), It.IsAny<string>()), Times.Once);
+    }
+
+    [Fact]
+    public void Encode_WithDifferentKeyValues_ThrowsKeyMismatchException()
+    {
+        SchemaItem<TestEntity, string> schemaItem = new("Name", true, _getter, _setter, _serializerMock.Object);
+        
+        TestEntity current = new()
+        {
+            Name = "CurrentValue"
+        };
+        
+        TestEntity previous = new()
+        {
+            Name = "PreviousValue"
+        };
+
+        Assert.Throws<KeyMismatchException>(() => schemaItem.Encode(_writer, current, previous));
+    }
+
+    [Fact]
+    public void Encode_WithIdenticalKeyValues_WritesData()
+    {
+        Mock<IDataBufferWriter> writerMock = new();
+
+        SchemaItem<TestEntity, string> schemaItem = new("Name", true, _getter, _setter, _serializerMock.Object);
+
+        string value = "SameValue";
+
+        TestEntity current = new() 
+        {
+            Name = value 
+        };
+
+        TestEntity previous = new() 
+        { 
+            Name = value
+        };
+
+        _serializerMock.Setup(s => s.GetEquals(It.Is<string>(v => v == value), It.Is<string>(v => v == value))).Returns(true);
+
+        schemaItem.Encode(writerMock.Object, current, previous);
+
+        _serializerMock.Verify(s => s.Encode(It.IsAny<IDataBufferWriter>(), It.Is<string>(v => v == value)), Times.Once);
+    }
 
     #endregion
 
-    #region Test Methods (GetEquals)
+    #region Nested Types
 
+    public class TestEntity
+    {
+        [Serialize(true)]
+        public string Name { get; set; }
+
+        public TestEntity()
+        {  
+            Name = "";
+        }
+    }
 
     #endregion
 }
