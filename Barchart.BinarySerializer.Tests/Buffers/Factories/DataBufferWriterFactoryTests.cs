09dc15f03c0e8887d0211b011a8c2a518f6cc53d
Luka Sotra <lukasotra@gmail.com>
2024-07-12 15:18:01 +0200

    Refactor: Folder restructuring.

--- Barchart.BinarySerializer.Tests/Buffers/Factories/DataBufferWriterFactoryTests.cs
+++ Barchart.BinarySerializer.Tests/Buffers/Factories/DataBufferWriterFactoryTests.cs
@@ -5,7 +5,7 @@ using Barchart.BinarySerializer.Buffers.Factories;
 
 #endregion
 
-namespace Barchart.BinarySerializer.Tests.Schemas.Factories;
+namespace Barchart.BinarySerializer.Tests.Bufers.Factories;
 
 public class DataBufferWriterFactoryTests
 {
