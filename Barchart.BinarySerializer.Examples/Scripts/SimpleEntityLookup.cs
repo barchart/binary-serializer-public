789e1896ef2e8eb19e4ee1c14a9525b5fb370207
Luka Sotra <lukasotra@gmail.com>
2024-07-16 11:33:31 +0200

    Refactor: Object creation change.

--- Barchart.BinarySerializer.Examples/Scripts/SimpleEntityLookup.cs
+++ Barchart.BinarySerializer.Examples/Scripts/SimpleEntityLookup.cs
@@ -2,6 +2,7 @@
 
 using Barchart.BinarySerializer.Buffers;
 using Barchart.BinarySerializer.Examples.Data;
+using Barchart.BinarySerializer.Schemas;
 using Barchart.BinarySerializer.Schemas.Factories;
 
 using Console = Barchart.BinarySerializer.Examples.Common.Console;
@@ -30,8 +31,8 @@ public class SimpleEntityLookup : IScript
 
         Console.WriteStep(ref step, "Populating the dictionary with two automobiles");
         
-        var delorian = new Automobile { Vin = "123-ABC", Make = "Delorian", Model = "DMC-12", Odometer = 0 };
-        var roadster = new Automobile { Vin = "789-XYZ", Make = "Tesla", Model = "Roadster", Odometer = 93000000 };
+        Automobile delorian = new() { Vin = "123-ABC", Make = "Delorian", Model = "DMC-12", Odometer = 0 };
+        Automobile roadster = new() { Vin = "789-XYZ", Make = "Tesla", Model = "Roadster", Odometer = 93000000 };
         
         automobiles.Add(delorian.Vin, delorian);
         automobiles.Add(roadster.Vin, roadster);
@@ -41,8 +42,8 @@ public class SimpleEntityLookup : IScript
         
         Console.WriteStep(ref step, "Generating a schema for the Automobile class (via reflection)");
         
-        var schemaFactory = new SchemaFactory();
-        var schema = schemaFactory.Make<Automobile>();
+        SchemaFactory schemaFactory = new();
+        ISchema<Automobile> schema = schemaFactory.Make<Automobile>();
 
         Console.WriteStep(ref step, "Processing an update for one of the existing automobiles");
         
@@ -54,7 +55,7 @@ public class SimpleEntityLookup : IScript
             0b00000000, 0b00000000
         };
 
-        var reader = new DataBufferReader(update);
+        DataBufferReader reader = new(update);
         
         Console.WriteStep(ref step, "Extracting the key value from the update");
         
@@ -62,7 +63,7 @@ public class SimpleEntityLookup : IScript
 
         Console.WriteStep(ref step, $"Key for the affected automobile instance is [ {key} ]");
         
-        var target = automobiles[key];
+        Automobile target = automobiles[key];
         
         Console.WriteStep(ref step, $"Updating automobile [ {key} ]");
         
