dce927fa6fa3ea463fb4037ad4d9c95c588483a0
Luka Sotra <lukasotra@gmail.com>
2024-07-19 19:47:47 +0200

    RefactoR: Add static method with `logFilePath` parameter to the `LoggerWrapper`.

--- Barchart.BinarySerializer/Logging/LoggerWrapper.cs
+++ Barchart.BinarySerializer/Logging/LoggerWrapper.cs
@@ -9,40 +9,72 @@ namespace Barchart.BinarySerializer.Logging;
 /// <summary>
 ///     Provides utility methods for logging using Serilog.
 /// </summary>
-public static class LoggerWrapper
+public class LoggerWrapper
 {
+    #region Fields
+
+    private static readonly LoggerWrapper? _instance = null;
+
+    #endregion
+
     #region Constructor(s)
 
-    static LoggerWrapper() {
-        string logFilePath = "../../../../Logs/log-.txt";
-        Log.Logger = new LoggerConfiguration()
-        .MinimumLevel.Debug()
-        .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
-        .CreateLogger();
-    }
+    private LoggerWrapper() {}
 
     #endregion
 
     #region Methods
 
+    /// <summary>
+    ///     Initializes the logger with a custom log file path.
+    /// </summary>
+    /// <param name="logFilePath">
+    ///     The path to the log file.
+    /// </param>
+    public static void Initialize(string logFilePath)
+    {
+        if (_instance != null)
+        {
+            throw new InvalidOperationException("Logger is already initialized.");
+        }
+
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+            .CreateLogger();
+
+    }
+
     public static void LogInformation(string text)
     {
+        EnsureInitialized();
         Log.Information(text);
     }
 
     public static void LogDebug(string text)
     {
+        EnsureInitialized();
         Log.Debug(text);
     }
 
     public static void LogWarning(string text)
     {
+        EnsureInitialized();
         Log.Warning(text);
     }
 
     public static void LogError(string text)
     {
+        EnsureInitialized();
         Log.Error(text);
+    }
+
+    private static void EnsureInitialized()
+    {
+        if (_instance == null)
+        {
+            throw new InvalidOperationException("Logger is not initialized. Call Initialize() with a log file path first.");
+        }
     }
 
     #endregion
