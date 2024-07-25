4564f5a059e3209738b87fa24dc1044d5bf4dd6b
Luka Sotra <lukasotra@gmail.com>
2024-07-25 11:41:48 +0200

    Feat: Implement `Serializer` class for making (de)serialization  process as simple as possible.

--- Barchart.BinarySerializer/Serializers/Serializer.cs
+++ Barchart.BinarySerializer/Serializers/Serializer.cs
@@ -1,6 +1,55 @@
+#region Using Statements
+
+using Barchart.BinarySerializer.Buffers;
+using Barchart.BinarySerializer.Buffers.Factories;
+using Barchart.BinarySerializer.Schemas;
+using Barchart.BinarySerializer.Schemas.Factories;
+
+#endregion
+
 namespace Barchart.BinarySerializer.Serializers;
 
-public class Serializer
+public class Serializer<T> where T : class, new()
 {
-    
+    #region Fields
+
+    private readonly SchemaFactory _schemaFactory;
+    private readonly ISchema<T> _schema;
+
+    private readonly DataBufferWriterFactory _writerFactory;
+    private readonly IDataBufferWriter _writer;
+
+    #endregion
+
+    #region Constructor(s)
+
+    public Serializer()
+    {
+        _schemaFactory = new();
+        _schema = _schemaFactory.Make<T>();
+
+        _writerFactory = new();
+        _writer = _writerFactory.Make();
+    }
+
+    #endregion
+
+    #region Methods
+
+    public byte[] Serialize(T source)
+    {
+        using(_writer.Bookmark())
+        {
+            return _schema.Serialize(_writer, source);
+        }
+    }
+
+    public T Deserialize(byte[] serialized)
+    {
+        DataBufferReader reader = new(serialized);
+        
+        return _schema.Deserialize(reader);
+    }
+
+    #endregion
 }
