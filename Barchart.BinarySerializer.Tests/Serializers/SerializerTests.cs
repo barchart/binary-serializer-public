60db71f0abc95025cab75289c82084f143d886c2
Luka Sotra <lukasotra@gmail.com>
2024-07-25 12:36:44 +0200

    Test: Add unit tests for `Serialize` class.

--- Barchart.BinarySerializer.Tests/Serializers/SerializerTests.cs
+++ Barchart.BinarySerializer.Tests/Serializers/SerializerTests.cs
@@ -1,6 +1,7 @@
 #region Using Statements
 
 using Barchart.BinarySerializer.Attributes;
+using Barchart.BinarySerializer.Serializers;
 
 #endregion
 
@@ -11,7 +12,8 @@ public class SerializerTests
     #region Fields
 
     private readonly ITestOutputHelper _testOutputHelper;
-
+    
+    private readonly Serializer<TestEntity> _serializer;
 
     #endregion
 
@@ -21,24 +23,98 @@ public class SerializerTests
     {
         _testOutputHelper = testOutputHelper;
 
+       _serializer = new();
         
     }
 
     #endregion
 
     #region Test Methods (Serializer)
-    
-    
-   
+
+    [Fact]
+    public void Serialize_ShouldReturnSerializedData()
+    {
+        TestEntity entity = new() 
+        { 
+            KeyProperty = "Key",
+            ValueProperty = "Value"
+        };
+
+        byte[] serialized = _serializer.Serialize(entity);
+
+        Assert.NotNull(serialized);
+        Assert.NotEmpty(serialized);
+    }
+
+    [Fact]
+    public void Serialize_WithPreviousEntity_ShouldReturnSerializedData()
+    {
+        TestEntity currentEntity = new() 
+        { 
+            KeyProperty = "Key", 
+            ValueProperty = "Value1" 
+        };
+
+        TestEntity previousEntity = new() 
+        { 
+            KeyProperty = "Key", 
+            ValueProperty = "Value0" 
+        };
+
+        byte[] serialized = _serializer.Serialize(currentEntity, previousEntity);
+
+        Assert.NotNull(serialized);
+        Assert.NotEmpty(serialized);
+    }
 
     #endregion
 
     #region Test Methods (Deserialize)
 
-    
+    [Fact]
+    public void Deserialize_ShouldReturnDeserializedEntity()
+    {
+        TestEntity entity = new() 
+        { 
+            KeyProperty = "Key", 
+            ValueProperty = "Value" 
+        };
+
+        byte[] serialized = _serializer.Serialize(entity);
+
+        TestEntity deserializedEntity = _serializer.Deserialize(serialized);
+
+        Assert.NotNull(deserializedEntity);
+        Assert.Equal(entity.KeyProperty, deserializedEntity.KeyProperty);
+        Assert.Equal(entity.ValueProperty, deserializedEntity.ValueProperty);
+    }
+
+    [Fact]
+    public void Deserialize_WithTarget_ShouldPopulateTargetEntity()
+    {
+        TestEntity entity = new() 
+        { 
+            KeyProperty = "Key", 
+            ValueProperty = "Value"
+        };
+
+        byte[] serialized = _serializer.Serialize(entity);
+        TestEntity targetEntity = new()
+        {
+            KeyProperty = "Key",
+            ValueProperty = ""
+        };
+
+        TestEntity deserializedEntity = _serializer.Deserialize(serialized, targetEntity);
+
+        Assert.NotNull(deserializedEntity);
+        Assert.Equal(targetEntity, deserializedEntity);
+        Assert.Equal(entity.KeyProperty, deserializedEntity.KeyProperty);
+        Assert.Equal(entity.ValueProperty, deserializedEntity.ValueProperty);
+    }
 
     #endregion
-
+    
     #region Nested Types
 
     public class TestEntity
