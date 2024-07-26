712594c1af084eebc39e9916875c5520ee931905
Luka Sotra <lukasotra@gmail.com>
2024-07-26 13:23:55 +0200

    Refactor: Docs.

--- Barchart.BinarySerializer/Buffers/Factories/DataBufferReaderFactory.cs
+++ Barchart.BinarySerializer/Buffers/Factories/DataBufferReaderFactory.cs
@@ -1,9 +1,14 @@
 namespace Barchart.BinarySerializer.Buffers.Factories;
 
+/// <summary>
+///     An implementation of <see cref="IDataBufferReaderFactory" /> that uses byte arrays 
+///     as the underlying binary data storage foreach <see cref="IDataBufferReader" />.
+/// </summary>
 public class DataBufferReaderFactory : IDataBufferReaderFactory
 {
     #region Methods
 
+    /// <inheritdoc />
     public IDataBufferReader Make(byte[] byteArray)
     {
         return new DataBufferReader(byteArray);
--- Barchart.BinarySerializer/Buffers/Factories/IDataBufferReaderFactory.cs
+++ Barchart.BinarySerializer/Buffers/Factories/IDataBufferReaderFactory.cs
@@ -1,9 +1,15 @@
 namespace Barchart.BinarySerializer.Buffers.Factories;
 
+/// <summary>
+///     A strategy for creating <see cref="IDataBufferReader" /> instances.
+/// </summary>
 public interface IDataBufferReaderFactory
 {
     #region Methods
 
+    /// <summary>
+    ///     Creates an <see cref="IDataBufferReader" /> instance.
+    /// </summary>
     IDataBufferReader Make(byte[] byteArray);
 
     #endregion
