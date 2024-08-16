7381d201e8a07bdf81e371b971a470d55a2ae15c
Bryan Ingle <bryaningle@gmail.com>
2024-08-15 19:32:10 -0600

    Fix: Undo previous commit (code was correct, missing flag is written by `Encode` method)

--- Barchart.BinarySerializer/Schemas/SchemaItem.cs
+++ Barchart.BinarySerializer/Schemas/SchemaItem.cs
@@ -80,14 +80,13 @@ public class SchemaItem<TEntity, TMember> : ISchemaItem<TEntity, TMember> where 
             throw new KeyMismatchException(typeof(TEntity), Name, true);
         }
 
-        if (!Key)
-        {
-            WriteMissingFlag(writer, valuesAreEqual);
-        }
-        
         if (Key || !valuesAreEqual)
         {
             Encode(writer, current);
+        }
+        else
+        {
+            WriteMissingFlag(writer, true); 
         }
     }
 
