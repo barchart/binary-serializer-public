dcc01485d96d906a886bb0c6fe948648b483eda6
Luka Sotra <lukasotra@gmail.com>
2024-08-20 15:53:54 +0200

    Refactor: Add docs.

--- Barchart.BinarySerializer/Schemas/Headers/IBinaryHeaderSerializer.cs
+++ Barchart.BinarySerializer/Schemas/Headers/IBinaryHeaderSerializer.cs
@@ -6,12 +6,44 @@ using Barchart.BinarySerializer.Buffers;
 
 namespace Barchart.BinarySerializer.Schemas.Headers;
 
+/// <summary>
+///     Defines the binary header serializer.
+/// </summary>
+/// <remarks>
+///     Implementations of this interface are responsible for encoding and decoding
+///     headers in the binary data. A header typically contains metadata such as an entity ID
+///     and snapshot information that is used to interpret the serialized data.
+/// </remarks>
 public interface IBinaryHeaderSerializer
 {
     #region Methods
-
+    
+    /// <summary>
+    ///     Serializes a header into the provided data buffer writer.
+    /// </summary>
+    /// <param name="writer">
+    ///     The data buffer writer to which the header will be written.
+    /// </param>
+    /// <param name="entityId">
+    ///     The entity ID to be included in the header. This ID helps identify the type of entity
+    ///     the data represents.
+    /// </param>
+    /// <param name="snapshot">
+    ///     A boolean value indicating whether the data represents a snapshot. If true, the
+    ///     snapshot flag will be set in the header.
+    /// </param>
     void Encode(IDataBufferWriter writer, byte entityId, bool snapshot);
     
+    /// <summary>
+    ///     Deserializes a header from the provided data buffer reader.
+    /// </summary>
+    /// <param name="reader">
+    ///     The data buffer reader from which the header will be read.
+    /// </param>
+    /// <returns>
+    ///     An <see cref="IHeader"/> instance representing the decoded header. The header includes
+    ///     information such as the entity ID and whether the data is a snapshot.
+    /// </returns>
     IHeader Decode(IDataBufferReader reader);
     
     #endregion
