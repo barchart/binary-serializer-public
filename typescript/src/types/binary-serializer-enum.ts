61df1333ad8e123c8e8cd6eebb58b3e44fb06203
Luka Sotra <lukasotra@gmail.com>
2024-12-09 13:25:13 +0100

    refactor: update enum serializer (use `Enum` from common-js library)

--- typescript/src/types/binary-serializer-enum.ts
+++ typescript/src/types/binary-serializer-enum.ts
@@ -1,6 +1,7 @@
 import { DataReader } from '../buffers/data-reader.interface';
 import { DataWriter } from '../buffers/data-writer.interface';
 import { BinaryTypeSerializer } from './binary-type-serializer.interface';
+import Enum from '@barchart/common-js/lang/Enum';
 
 /**
  *  Reads (and writes) enumeration values to (and from) a binary data source.
@@ -10,27 +11,29 @@ import { BinaryTypeSerializer } from './binary-type-serializer.interface';
  * @implements {BinaryTypeSerializer<T>}
  * @template T - The enumeration type.
  */
-export class BinarySerializerEnum<T extends number> implements BinaryTypeSerializer<T> {
+export class BinarySerializerEnum<T extends Enum> implements BinaryTypeSerializer<T> {
     get sizeInBytes(): number {
         return 4;
     }
 
     private readonly binarySerializerInt: BinaryTypeSerializer<number>;
+    private readonly enumType: new (...args: any[]) => T;
 
-    constructor(binarySerializerInt: BinaryTypeSerializer<number>) {
+    constructor(binarySerializerInt: BinaryTypeSerializer<number>, enumType: new (...args: any[]) => T) {
         this.binarySerializerInt = binarySerializerInt;
+        this.enumType = enumType;
     }
 
     encode(writer: DataWriter, value: T): void {
-        this.binarySerializerInt.encode(writer, value);
+        this.binarySerializerInt.encode(writer, value.mapping as number);
     }
 
     decode(reader: DataReader): T {
         const intValue = this.binarySerializerInt.decode(reader);
-        return intValue as T;
+        return Enum.fromMapping(this.enumType, intValue) as T;
     }
 
     getEquals(a: T, b: T): boolean {
-        return this.binarySerializerInt.getEquals(a, b);
+        return this.binarySerializerInt.getEquals(a.mapping as number, b.mapping as number);
     }
 }
